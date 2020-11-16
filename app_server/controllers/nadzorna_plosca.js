21575353e73816950b45ac7773f008def9b9be04
te2445 <36800093+te2445@users.noreply.github.com>
2020-11-16 01:37:07 +0100

    nadzorna-plosca nav menu helper

--- app_server/controllers/nadzorna_plosca.js
+++ app_server/controllers/nadzorna_plosca.js
@@ -3,11 +3,11 @@ const menu=function(req,res){
 }
 
 const rezervacije=function(req,res){
-    res.render('nadzorna_plosca_rezervacije',{layout:'layout_nadzorna_plosca.hbs',title:'Nadzorna plošča - Rezervacije'})
+    res.render('nadzorna_plosca_rezervacije',{layout:'layout_nadzorna_plosca.hbs',title:'Nadzorna plošča - Rezervacije',zaposleni_role:req.query.vloga})
 }
 
 const urnik=function(req,res){
-    res.render('nadzorna_plosca_urnik',{layout:'layout_nadzorna_plosca.hbs',title:'Nadzorna plošča - Urnik'})
+    res.render('nadzorna_plosca_urnik',{layout:'layout_nadzorna_plosca.hbs',title:'Nadzorna plošča - Urnik',zaposleni_role:req.query.vloga})
 }
 
 module.exports={
--- app_server/views/helpers/hbsh.js
+++ app_server/views/helpers/hbsh.js
@@ -8,3 +8,30 @@ hbs.registerHelper('izbrano', (izbrano_ime) => {
   res+='<li id="rezerviraj_mizo"'+ (izbrano_ime=='rezerviraj_mizo' ? ' class="selected"' : '') +'> <a href="./rezervacija.html">REZERVIRAJ MIZO</a></li>';
   return res;
 });
+
+hbs.registerHelper('nadzorna_plosca_menu_by_role', (zaposleni_role) => {
+  //urnik imajo vsi
+  //rezervacije,narocila_strezba,narocila_kuhinja,meni,zaloga,zasluzek,zaposleni
+  var vodja= [1,1,1,1,1,1,1];
+  var natakar= [1,1,0,0,0,0,0];
+  var kuhar= [0,0,1,1,1,0,0];
+  var racunovodja= [0,0,0,0,0,1,1];
+  var vloga;
+  if (!zaposleni_role) {
+    vloga=vodja;
+  } else {
+    vloga=vodja;
+    zaposleni_role.localeCompare("vodja") == 0 ? vloga=vodja : vloga=vloga;
+    zaposleni_role.localeCompare("natakar") == 0 ? vloga=natakar : vloga=vloga;
+    zaposleni_role.localeCompare("kuhar") == 0 ? vloga=kuhar : vloga=vloga;
+    zaposleni_role.localeCompare("racunovodja") == 0 ? vloga=racunovodja : vloga=vloga;
+  }
+  var meni_sestavljen = (vloga[0] == 1 ? '<a href="/nadzorna_plosca/rezervacije"><i class="fas fa-user-clock ikone-stil-posamezna"></i></a>' : '');
+  meni_sestavljen += vloga[1] == 1 ? '<a href="nadzorna_plosca_narocila_natakar.html"><i class="fas fa-utensils ikone-stil-posamezna"></i></a>' : '';
+  meni_sestavljen += vloga[2] == 1 ? '<a href="nadzorna_plosca_narocila_kuhar.html"><i class="fas fa-utensils ikone-stil-posamezna"></i></a>' : '';
+  meni_sestavljen += vloga[3] == 1 ? '<a href="nadzorna_plosca_meni.html"><i class="fas fa-book-open ikone-stil-posamezna"></i></a>' : '';
+  meni_sestavljen += vloga[4] == 1 ? '<a href="nadzorna_plosca_zaloga.html"><i class="fas fa-boxes ikone-stil-posamezna"></i></a>' : '';
+  meni_sestavljen += vloga[5] == 1 ? '<a href="nadzorna_plosca_zasluzek.html"><i class="fas fa-coins ikone-stil-posamezna"></i></a>' : '';
+  meni_sestavljen += vloga[6] == 1 ? '<a href="nadzorna_plosca_zaposleni.html"><i class="fas fa-user-cog ikone-stil-posamezna"></i></a>' : '';
+  return meni_sestavljen;
+});
