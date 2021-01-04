2cf834b1db8b539d71f0db2825e436bb0a26ad77
ZigaPozar <zp1399@student.uni-lj.si>
2021-01-04 02:12:30 +0100

    Testi na nadzorni plošči

--- app_public/src/app/skupno/storitve/zaloga.service.ts
+++ app_public/src/app/skupno/storitve/zaloga.service.ts
@@ -15,7 +15,7 @@ export class ZalogaService {
   private api_url = environment.api_url;
 
   private initHeaders(): HttpHeaders {
-    return  new HttpHeaders().set('Authorization', 'Bearer ' + this.authService.vrniZeton());
+    return new HttpHeaders().set('Authorization', 'Bearer ' + this.authService.vrniZeton());
   }
 
   public pridobiSestavine(query: any): Observable<any>{
@@ -23,37 +23,16 @@ export class ZalogaService {
   }
 
   public dodajSestavino(sestavina: any): Promise<Zaloga>{
-    /*
-    const httpLastnosti = {
-      headers: new HttpHeaders({
-        'Authorization': `Bearer ${this.shramba.getItem('edugeocache-zeton')}`
-      })
-    };
-     *//*                                                    , httpLastnosti) */
     return this.http.post(this.api_url + '/zaloga', sestavina).toPromise().then(odgovor => odgovor as Zaloga)
       .catch(napaka => this.obdelajNapako(napaka));
   }
 
   public posodobiSestavino(sestavina: any): Promise<Zaloga>{
-    /*
-    const httpLastnosti = {
-      headers: new HttpHeaders({
-        'Authorization': `Bearer ${this.shramba.getItem('edugeocache-zeton')}`
-      })
-    };
-     *//*                                                      , httpLastnosti) */
     return this.http.put(this.api_url + '/zaloga', sestavina).toPromise().then(odgovor => odgovor as Zaloga)
       .catch(napaka => this.obdelajNapako(napaka));
   }
 
   public odstraniSestavino(id: string): Promise<any>{
-    /*
-    const httpLastnosti = {
-      headers: new HttpHeaders({
-        'Authorization': `Bearer ${this.shramba.getItem('edugeocache-zeton')}`
-      })
-    };
-     *//*                                                      , httpLastnosti) */
     return this.http.delete(this.api_url + '/zaloga/' + id).toPromise().then(null)
       .catch(napaka => this.obdelajNapako(napaka));
   }
--- test/Aldente.test.js
+++ test/Aldente.test.js
@@ -61,6 +61,88 @@
 
         });
 
+        describe("Nadzorna plošča", function () {
+            this.timeout(30 * 1000);
+            before(() => {
+                brskalnik.get(aplikacijaUrl + 'nadzorna_plosca');
+            });
+            it("Urnik", async () => {
+                await pocakajStranNalozena(brskalnik, 10, '//h1');
+                const povezava = await brskalnik.findElement(By.xpath("//a[contains(text(), 'urnik')]"));
+                expect(povezava).to.not.be.empty;
+                await povezava.click();
+            });
+            context("ustreznost podatkov na strani z urniki", function() {
+                it("naslov strani urniki", async function() {
+                    await pocakajStranNalozena(brskalnik, 10, "//h1");
+                    let naslov = await brskalnik.findElement(By.css("h1"));
+                    expect(naslov).to.not.be.empty;
+                    await naslov.getText().then(function(vsebina) {
+                        expect(vsebina).to.be.equal("Urnik izmen");
+                    });
+                });
+            });
+            context("ustreznost podatkov na strani z rezervacijami", function() {
+                it("naslov strani rezervacije", async function() {
+                    await pocakajStranNalozena(brskalnik, 10, "//h1");
+                    let naslov = await brskalnik.findElement(By.css("h1"));
+                    expect(naslov).to.not.be.empty;
+                    await naslov.getText().then(function(vsebina) {
+                        expect(vsebina).to.be.equal("Upravljanje Rezervacij");
+                    });
+                });
+            });
+            context("ustreznost podatkov na strani z menijem", function() {
+                it("naslov strani meni", async function() {
+                    await pocakajStranNalozena(brskalnik, 10, "//h1");
+                    let naslov = await brskalnik.findElement(By.css("button"));
+                    expect(naslov).to.not.be.empty;
+                    await naslov.getText().then(function(vsebina) {
+                        expect(vsebina).to.be.equal(" DODAJ");
+                    });
+                });
+            });
+            context("ustreznost podatkov na strani z zalogo", function() {
+                it("naslov strani zaloga", async function() {
+                    await pocakajStranNalozena(brskalnik, 10, "//h1");
+                    let naslov = await brskalnik.findElement(By.css("h1"));
+                    expect(naslov).to.not.be.empty;
+                    await naslov.getText().then(function(vsebina) {
+                        expect(vsebina).to.be.equal("Zaloga");
+                    });
+                });
+            });
+            context("ustreznost podatkov na strani z zaslužkom", function() {
+                it("naslov strani zaslužek", async function() {
+                    await pocakajStranNalozena(brskalnik, 10, "//h1");
+                    let naslov = await brskalnik.findElement(By.css("h1"));
+                    expect(naslov).to.not.be.empty;
+                    await naslov.getText().then(function(vsebina) {
+                        expect(vsebina).to.be.equal("Zasluzek");
+                    });
+                });
+            });
+            context("ustreznost podatkov na strani z zaposlenimi", function() {
+                it("naslov strani zaposleni", async function() {
+                    await pocakajStranNalozena(brskalnik, 10, "//h1");
+                    let naslov = await brskalnik.findElement(By.css("h1"));
+                    expect(naslov).to.not.be.empty;
+                    await naslov.getText().then(function(vsebina) {
+                        expect(vsebina).to.be.equal("Zaposleni");
+                    });
+                });
+            });
+        });
+
+        describe("Dodajanje sestavine", async function() {
+            this.timeout(30 * 1000);
+            before(async function() {
+                await brskalnik.get(aplikacijaUrl + 'nadzorna_plosca/zaloga');
+            });
+            it('odpri modal dodaj', function () {
+                // TODO
+            });
+        });
 
     } catch (napaka) {
         console.log("Med testom je prišlo do napake!");
