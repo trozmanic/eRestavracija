71737307514139cd9c94ba8852ec383247217f9e
seenke <azurkurbasic@gmail.com>
2021-01-04 03:41:46 +0100

    Added more testing

--- test/Aldente.test.js
+++ test/Aldente.test.js
@@ -48,6 +48,22 @@
                 .build();
         });
 
+        describe("Resetriaj bazo", function () {
+            this.timeout(30 * 1000);
+            before(() => {brskalnik.get(aplikacijaUrl + "db");})
+            it('resetiranje podatkovne baze', async function () {
+                await pocakajStranNalozena(brskalnik, 10, '//button');
+                const setDB = await brskalnik.findElement(By.id('setDB'));
+                const dropDB = await brskalnik.findElement(By.id('dropDB'));
+                expect(setDB).not.to.be.empty;
+                expect(dropDB).not.to.be.empty;
+
+                await dropDB.click();
+                await setDB.click();
+
+            })
+        })
+
         describe("Ustreznost pristajalne strani za neprijavljenega uporabnika", function () {
             this.timeout(30*1000);
             before(() => {brskalnik.get(aplikacijaUrl);})
@@ -75,14 +91,14 @@
             this.timeout(30 * 1000);
             before(async function() { await brskalnik.get(aplikacijaUrl); });
 
-            it("izbriši uporabnika iz podatkovne baze", async function() {
-                let dockerAtlasBrisiUporabnika = 'docker exec -i AlDente-mongoDB bash -c mongo \' \
-                \ use AlDente; \
-                \ db.Uporabnik.remove({email_naslov:"testic@aldente.si}); \ ';
-                exec(dockerAtlasBrisiUporabnika).on("close", (koda) => {
-                    expect(koda).to.be.equal(0);
-                });
-            });
+            // it("izbriši uporabnika iz podatkovne baze", async function() {
+            //     let dockerAtlasBrisiUporabnika = 'docker exec -i AlDente-mongoDB bash -c mongo \' \
+            //     \ use AlDente; \
+            //     \ db.Uporabnik.remove({email_naslov:"testic@aldente.si}); \ ';
+            //     exec(dockerAtlasBrisiUporabnika).on("close", (koda) => {
+            //         expect(koda).to.be.equal(0);
+            //     });
+            // });
 
             it("registracija klik", async function() {
                 let povezava = await brskalnik.findElement(
@@ -116,16 +132,97 @@
                 let ponoviGeslo = await brskalnik.findElement(By.id('InputPasswordRepeat'));
                 ponoviGeslo.sendKeys("test");
 
-                brskalnik.findElement(
+                await brskalnik.findElement(
                     By.xpath("//button[contains(text(), 'REGISTRIRAJ SE')]")).click();
+
+                const modal = await brskalnik.findElement(By.className('swal2-confirm'))
+                modal.click();
+            });
+
+            it("preveri ali je uporabnik prijavljen", async function() {
+                await pocakajStranNalozena(brskalnik, 10, "//h1");
+                let uporabnik = await brskalnik.findElement(
+                    By.xpath("//p[contains(text(), 'testic@aldente.si')]"));
+                expect(uporabnik).to.not.be.empty;
             });
 
             it("pridobi JWT žeton", async function() {
                 jwtZeton = await brskalnik.executeScript(function() {
                     return localStorage.getItem("token");
                 });
+                console.log(jwtZeton)
                 expect(jwtZeton).to.not.be.empty;
             });
+        })
+
+        describe('Dodajanje ocene', async function () {
+            this.timeout(30 * 1000);
+            before(async function() { await brskalnik.get(aplikacijaUrl + "meni"); });
+            it('preveri ce je jed na meniju', async function () {
+                await pocakajStranNalozena(brskalnik, 10, "//h1");
+                let jed = await brskalnik.findElements(By.className('card'))
+                expect(jed).to.be.an('array').of.length(6);
+
+                let buttons = await brskalnik.findElements(By.className('button'))
+                await buttons[0].click();
+            })
+            it('oceni jed', async function () {
+                let posljiButton = await brskalnik.findElement(By.xpath("//button[contains(text(), 'POSLJI')]"))
+                await posljiButton.click();
+                const modal = await brskalnik.findElement(By.css('.swal2-confirm'))
+                expect(modal).to.not.be.empty;
+                await modal.click();
+            })
+            it('preveri ce je ocena bila oddana', async function () {
+                let tekst = await brskalnik.findElement(By.xpath("//h2[contains(text(), 'TO JED STE ZE OCENILI')]"))
+                expect(tekst).to.not.be.empty;
+            })
+        })
+
+        describe ('Odjava in prijava uporabnika', async function () {
+            this.timeout(30 * 1000);
+            before(async function() { await brskalnik.get(aplikacijaUrl); });
+
+                it('odjavi uporabnika', async function () {
+                    let button = await brskalnik.findElement(By.xpath("//button[contains(text(), 'ODJAVA')]"))
+                    await button.click();
+                    expect(button).to.not.be.empty;
+                    let povezava = await brskalnik.findElement(
+                        By.xpath("//button[contains(text(), 'REGISTRACIJA')]"));
+                    expect(povezava).to.not.be.empty;
+                })
+
+                it('prijavi uporabnika', async function () {
+                    let povezava = await brskalnik.findElement(
+                        By.xpath("//button[contains(text(), 'PRIJAVA')]"));
+                    await povezava.click();
+
+                    let email = await brskalnik.findElement(
+                        By.id('exampleInputEmail1'));
+                    expect(email).to.not.be.empty;
+                    email.sendKeys("testic@aldente.si");
+
+                    let geslo = await brskalnik.findElement(
+                        By.id('exampleInputPassword1'));
+                    expect(geslo).to.not.be.empty;
+                    geslo.sendKeys("test");
+
+                    let povezavaLogin = await brskalnik.find(
+                        By.xpath("//button[contains(text(), 'PRIJAVI SE')]")
+                    )
+                    expect(povezavaLogin).to.not.be.empty;
+                    await povezavaLogin.click();
+                })
+
+                it("pridobi JWT žeton", async function() {
+                jwtZeton = await brskalnik.executeScript(function() {
+                    return localStorage.getItem("token");
+                });
+                console.log(jwtZeton)
+                expect(jwtZeton).to.not.be.empty;
+                });
+
+
         })
 
 
